0501ea9c40914dc200f1e298dd30835e0f930dfc
mofumofuchan <you_oki1@yahoo.co.jp>
2020-10-07 23:48:41 +0900

    カラム間のタスクの移動を実装

--- src/column.js
+++ src/column.js
@@ -7,12 +7,18 @@ const Container = styled.div`
 margin: 8px;
 border: 1px solid lightgrey;
 border-radius: 2px;
+width: 220px;
+
+display: flex;
+flex-direction: column;
 `;
 const Title = styled.h3`
 padding: 8px;
 `;
 const TaskList = styled.div`
 padding: 8px;
+flex-grow: 1;
+min-height: 100px;
 `;
 
 export default class Column extends React.Component {
