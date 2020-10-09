36e4689e24c622ef84a2025b409292fb0e8e6adc
mofumofuchan <you_oki1@yahoo.co.jp>
2020-10-09 23:54:34 +0900

    ドラッグ中にタスクの色を変える処理を追加

--- src/task.js
+++ src/task.js
@@ -7,17 +7,19 @@ border: 1px solid lightgrey;
 border-radius: 2px;
 paddding: 8px;
 margin-buttom: 8px;
+background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
 `;
 
 export default class Task extends React.Component {
   render() {
     return (
       <Draggable draggableId={this.props.task.id} index={this.props.index}>
-        {(provided) => (
+        {(provided, snapshot) => (
           <Container
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
           >
             {this.props.task.content}
           </Container>
